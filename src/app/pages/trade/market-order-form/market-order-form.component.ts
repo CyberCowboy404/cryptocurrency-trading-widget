1bcc68edbbf78b5be64ce29d24e0204987c97d8b
cybercowboy404 <cybercowboy404@gmail.com>
2022-05-13 12:17:04 +0300

    clearup

--- src/app/pages/trade/market-order-form/market-order-form.component.ts
+++ src/app/pages/trade/market-order-form/market-order-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TickerService } from 'src/app/core/services/ticker.service';
 import { TradeService } from 'src/app/core/services/trade.service';
 import { SymbolTicker } from 'src/app/core/types/ticker';
